0b5806c7a0d44d172f2c6e02a8594c990a914e04
kristinaSipka <kristinashipka@ymail.com>
2019-02-12 14:55:06 +0100

    Created MainTabBarController

---
+++ Party/MainTabController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabController.swift
+//  Party
+//
+//  Created by mac on 2/12/19.
+//  Copyright © 2019 Kristina. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
