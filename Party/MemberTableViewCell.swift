06a43db49268ce9581b8980bfa5a6c23dbaa97db
kristinaSipka <kristinashipka@ymail.com>
2019-02-12 14:57:06 +0100

    Created PartyTableViewCell, PartyViewController, MemberTableViewCell, MemberViewController and add outlets

---
+++ Party/MemberTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MemberTableViewCell.swift
+//  Party
+//
+//  Created by mac on 2/11/19.
+//  Copyright © 2019 Kristina. All rights reserved.
+//
+
+import UIKit
+
+class MemberTableViewCell: UITableViewCell {
+
+    
+    
+    @IBOutlet weak var memberImage: UIImageView!
+    
+    @IBOutlet weak var memberName: UILabel!
+}
---
+++ Party/PartyTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PartyTableViewCell.swift
+//  Party
+//
+//  Created by mac on 2/11/19.
+//  Copyright © 2019 Kristina. All rights reserved.
+//
+
+import UIKit
+
+class PartyTableViewCell: UITableViewCell {
+
+  
+    @IBOutlet weak var partyDesc: UILabel!
+    @IBOutlet weak var partyDate: UILabel!
+    @IBOutlet weak var partyName: UILabel!
+}
